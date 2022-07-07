ce78f3bc0b19989dd047f90b109c361efc8fe5fa
thomaschhh <holz.thomas@yahoo.de>
2022-07-07 12:14:19 +0200

    Added placeholder components

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,13 @@ SimpleMBCompAudioProcessorEditor::SimpleMBCompAudioProcessorEditor (SimpleMBComp
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    
+    addAndMakeVisible(controlBar);
+    addAndMakeVisible(analyzer);
+    addAndMakeVisible(globalControls);
+    addAndMakeVisible(bandControls);
+    
+    setSize (600, 500);
 }
 
 SimpleMBCompAudioProcessorEditor::~SimpleMBCompAudioProcessorEditor()
@@ -37,4 +43,10 @@ void SimpleMBCompAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    auto bounds = getLocalBounds();
+    
+    controlBar.setBounds(bounds.removeFromTop(32));
+    bandControls.setBounds(bounds.removeFromBottom(135));
+    analyzer.setBounds(bounds.removeFromTop(225));
+    globalControls.setBounds(bounds);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -12,6 +12,22 @@
 #include "PluginProcessor.h"
 
 //==============================================================================
+struct Placeholder : juce::Component
+{
+    Placeholder()
+    {
+        juce::Random r;
+        customColor = juce::Colour(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+    };
+    
+    void paint (juce:: Graphics& g) override
+    {
+        g.fillAll(customColor);
+    }
+    
+    juce:: Colour customColor;
+};
+
 /**
 */
 class SimpleMBCompAudioProcessorEditor  : public juce::AudioProcessorEditor
@@ -28,6 +44,9 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleMBCompAudioProcessor& audioProcessor;
-
+    
+    // These are the four different sections of the GUI
+    Placeholder controlBar, analyzer, globalControls, bandControls;
+    
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessorEditor)
 };
