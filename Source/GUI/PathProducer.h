0b342630075dac5e39e6ffc2e09f02dfeb5cb1bd
thomaschhh <holz.thomas@yahoo.de>
2022-07-11 16:09:28 +0200

    Separate files refactor

---
+++ Source/GUI/PathProducer.h
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+    PathProducer.h
+    Created: 11 Jul 2022 3:51:11pm
+    Author:  Thomas Holz
+
+  ==============================================================================
+*/
+
+#pragma once
+#include <JuceHeader.h>
+#include "FFTDataGenerator.h"
+#include "AnalyzerPathGenerator.h"
+#include "../PluginProcessor.h"
+
+struct PathProducer
+{
+    PathProducer(SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>& scsf) :
+    leftChannelFifo(&scsf)
+    {
+        leftChannelFFTDataGenerator.changeOrder(FFTOrder::order2048);
+        monoBuffer.setSize(1, leftChannelFFTDataGenerator.getFFTSize());
+    }
+    void process(juce::Rectangle<float> fftBounds, double sampleRate);
+    juce::Path getPath() { return leftChannelFFTPath; }
+private:
+    SingleChannelSampleFifo<SimpleMBCompAudioProcessor::BlockType>* leftChannelFifo;
+    
+    juce::AudioBuffer<float> monoBuffer;
+    
+    FFTDataGenerator<std::vector<float>> leftChannelFFTDataGenerator;
+    
+    AnalyzerPathGenerator<juce::Path> pathProducer;
+    
+    juce::Path leftChannelFFTPath;
+};
---
+++ Source/GUI/SpectrumAnalyzer.cpp
@@ -0,0 +1,227 @@
+/*
+  ==============================================================================
+
+    SpectrumAnalyzer.cpp
+    Created: 11 Jul 2022 3:50:54pm
+    Author:  Thomas Holz
+
+  ==============================================================================
+*/
+
+#include "SpectrumAnalyzer.h"
+
+
+SpectrumAnalyzer::SpectrumAnalyzer(SimpleMBCompAudioProcessor& p) :
+audioProcessor(p),
+leftPathProducer(audioProcessor.leftChannelFifo),
+rightPathProducer(audioProcessor.rightChannelFifo)
+{
+    const auto& params = audioProcessor.getParameters();
+    for( auto param : params )
+    {
+        param->addListener(this);
+    }
+    
+    startTimerHz(60);
+}
+
+SpectrumAnalyzer::~SpectrumAnalyzer()
+{
+    const auto& params = audioProcessor.getParameters();
+    for( auto param : params )
+    {
+        param->removeListener(this);
+    }
+}
+
+void SpectrumAnalyzer::paint (juce::Graphics& g)
+{
+    using namespace juce;
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (Colours::black);
+
+    drawBackgroundGrid(g);
+    
+    auto responseArea = getAnalysisArea();
+    
+    if( shouldShowFFTAnalysis )
+    {
+        auto leftChannelFFTPath = leftPathProducer.getPath();
+        leftChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
+        
+        g.setColour(Colour(97u, 18u, 167u)); //purple-
+        g.strokePath(leftChannelFFTPath, PathStrokeType(1.f));
+        
+        auto rightChannelFFTPath = rightPathProducer.getPath();
+        rightChannelFFTPath.applyTransform(AffineTransform().translation(responseArea.getX(), responseArea.getY()));
+        
+        g.setColour(Colour(215u, 201u, 134u));
+        g.strokePath(rightChannelFFTPath, PathStrokeType(1.f));
+    }
+        
+    Path border;
+    
+    border.setUsingNonZeroWinding(false);
+    
+    border.addRoundedRectangle(getRenderArea(), 4);
+    border.addRectangle(getLocalBounds());
+    
+    g.setColour(Colours::black);
+    
+    g.fillPath(border);
+    
+    drawTextLabels(g);
+    
+    g.setColour(Colours::orange);
+    g.drawRoundedRectangle(getRenderArea().toFloat(), 4.f, 1.f);
+}
+
+std::vector<float> SpectrumAnalyzer::getFrequencies()
+{
+    return std::vector<float>
+    {
+        20, /*30, 40,*/ 50, 100,
+        200, /*300, 400,*/ 500, 1000,
+        2000, /*3000, 4000,*/ 5000, 10000,
+        20000
+    };
+}
+
+std::vector<float> SpectrumAnalyzer::getGains()
+{
+    return std::vector<float>
+    {
+        -24, -12, 0, 12, 24
+    };
+}
+
+std::vector<float> SpectrumAnalyzer::getXs(const std::vector<float> &freqs, float left, float width)
+{
+    std::vector<float> xs;
+    for( auto f : freqs )
+    {
+        auto normX = juce::mapFromLog10(f, 20.f, 20000.f);
+        xs.push_back( left + width * normX );
+    }
+    
+    return xs;
+}
+
+void SpectrumAnalyzer::drawBackgroundGrid(juce::Graphics &g)
+{
+    using namespace juce;
+    auto freqs = getFrequencies();
+    
+    auto renderArea = getAnalysisArea();
+    auto left = renderArea.getX();
+    auto right = renderArea.getRight();
+    auto top = renderArea.getY();
+    auto bottom = renderArea.getBottom();
+    auto width = renderArea.getWidth();
+    
+    auto xs = getXs(freqs, left, width);
+    
+    g.setColour(Colours::dimgrey);
+    for( auto x : xs )
+    {
+        g.drawVerticalLine(x, top, bottom);
+    }
+    
+    auto gain = getGains();
+    
+    for( auto gDb : gain )
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+        
+        g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::darkgrey );
+        g.drawHorizontalLine(y, left, right);
+    }
+}
+
+void SpectrumAnalyzer::drawTextLabels(juce::Graphics &g)
+{
+    using namespace juce;
+    g.setColour(Colours::lightgrey);
+    const int fontHeight = 10;
+    g.setFont(fontHeight);
+    
+    auto renderArea = getAnalysisArea();
+    auto left = renderArea.getX();
+    
+    auto top = renderArea.getY();
+    auto bottom = renderArea.getBottom();
+    auto width = renderArea.getWidth();
+    
+    auto freqs = getFrequencies();
+    auto xs = getXs(freqs, left, width);
+    
+    for( int i = 0; i < freqs.size(); ++i )
+    {
+        auto f = freqs[i];
+        auto x = xs[i];
+
+        bool addK = false;
+        String str;
+        if( f > 999.f )
+        {
+            addK = true;
+            f /= 1000.f;
+        }
+
+        str << f;
+        if( addK )
+            str << "k";
+        str << "Hz";
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+
+        Rectangle<int> r;
+
+        r.setSize(textWidth, fontHeight);
+        r.setCentre(x, 0);
+        r.setY(1);
+        
+        g.drawFittedText(str, r, juce::Justification::centred, 1);
+    }
+    
+    auto gain = getGains();
+
+    for( auto gDb : gain )
+    {
+        auto y = jmap(gDb, -24.f, 24.f, float(bottom), float(top));
+        
+        String str;
+        if( gDb > 0 )
+            str << "+";
+        str << gDb;
+        
+        auto textWidth = g.getCurrentFont().getStringWidth(str);
+        
+        Rectangle<int> r;
+        r.setSize(textWidth, fontHeight);
+        r.setX(getWidth() - textWidth);
+        r.setCentre(r.getCentreX(), y);
+        
+        g.setColour(gDb == 0.f ? Colour(0u, 172u, 1u) : Colours::lightgrey );
+        
+        g.drawFittedText(str, r, juce::Justification::centredLeft, 1);
+        
+        str.clear();
+        str << (gDb - 24.f);
+
+        r.setX(1);
+        textWidth = g.getCurrentFont().getStringWidth(str);
+        r.setSize(textWidth, fontHeight);
+        g.setColour(Colours::lightgrey);
+        g.drawFittedText(str, r, juce::Justification::centredLeft, 1);
+    }
+}
+
+void SpectrumAnalyzer::resized()
+{
+}
+
+void SpectrumAnalyzer::parameterValueChanged(int parameterIndex, float newValue)
+{
+    parametersChanged.set(true);
+}
